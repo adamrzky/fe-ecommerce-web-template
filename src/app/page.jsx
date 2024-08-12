b68ad90fd2024cd6816ce782a6eeb9e7f63d337e
Teguh <teguhwahidin098@gmail.com>
2024-08-12 12:03:09 +0700

    Update Home Add product

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,37 @@
+"use client";
 import MainLayout from '@/components/MainLayout';
 import Banner from '@/components/Banner';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import baseUrl from '@/utils/constains';
+import ProductCard from '@/components/ProductCard';
 
 export default function Home() {
+  const router = useRouter();
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchResults = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}/product`, {
+        });
+        const data = response.data.data;
+        setResults(Array.isArray(data) ? data : []);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false); 
+      }
+    };
+
+    fetchResults();
+  }, []);
+
+  const HandleToProducts = () => {
+    router.push("/products");
+  };
   return (
     <>
       <MainLayout>
@@ -11,34 +41,44 @@ export default function Home() {
         >
           <Banner/>
         </section>
-        <section
+        {/* <section
           id='hero'
           className='mx-auto max-w-[1320px] my-8 px-10 xl:px-0'
         >
           <div className='bg-slate-300 md:h-[450px] h-80 rounded-md p-4'></div>
-        </section>
+        </section> */}
         <section
           id='product'
           className='mx-auto max-w-[1320px] my-8 px-10 xl:px-0'
         >
           <div className=''>
             <p className='text-[#3E3E3E] md:text-lg text-sm font-medium mb-2'>
-              Find things you'll love. Support independent sellers.
+            Product Templates.
             </p>
             <p className='text-[#9A9A9A] md:text-sm text-xs font-medium'>
-              Only on polka.
+            Find Your Perfect Template.
             </p>
-            <div className='grid grid-cols-2 gap-4 mt-4 md:grid-cols-5 md:gap-8 md:mt-8'>
-              <div className='p-4 rounded-md h-28 bg-slate-300'></div>
-              <div className='p-4 rounded-md h-28 bg-slate-300'></div>
-              <div className='p-4 rounded-md h-28 bg-slate-300'></div>
-              <div className='p-4 rounded-md h-28 bg-slate-300'></div>
-              <div className='p-4 rounded-md h-28 bg-slate-300'></div>
-              <div className='p-4 rounded-md h-28 bg-slate-300'></div>
-              <div className='p-4 rounded-md h-28 bg-slate-300'></div>
-              <div className='p-4 rounded-md h-28 bg-slate-300'></div>
-              <div className='p-4 rounded-md h-28 bg-slate-300'></div>
-              <div className='p-4 rounded-md h-28 bg-slate-300'></div>
+            {loading ? ( 
+              <div className='basis-3/4'>
+                <p>Loading...</p>
+              </div>
+            ) : results.length > 0 ? ( 
+              <div className='basis-3/4'>
+                <div className='grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 2xl:gap-5'>
+                  {results.slice(0, 3).map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                  ))}
+                </div>
+              </div>
+            ) : ( 
+              <div className='basis-3/4'>
+                <p>No results found</p>
+              </div>
+            )}
+            <div className="w-full text-center">
+              <button className="my-10 bg-[#EB8426] hover:bg-[#EB6D20] px-7 py-3 text-white text-sm font-medium rounded-full" onClick={HandleToProducts} >
+                View More
+              </button>
             </div>
           </div>
         </section>
